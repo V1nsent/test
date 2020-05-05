88ca87edf7ed9421a3a83d9566a0763330231591
Виктор Андреев <viktorandreev@MacBook-Pro-Viktor.local>
2020-05-05 16:17:54 +0300

    второй коммит

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+//var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+// console.log(pi);
+// let answer = prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(+null));
+
+    let incr = true,
+    decr = true;
+
+    // console.log(incr++);
+    // console.log(incr);
+
+    console.log(incr || decr);
